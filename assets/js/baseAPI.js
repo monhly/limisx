c6f5a9b08ca13b125bc5c8ce2b8ab06a0a7e32e8
monhly <61449492+monhly@users.noreply.github.com>
2020-02-29 09:36:43 +0800

    后台主页的完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,17 +4,19 @@ $.ajaxPrefilter(function (option) {
     // 进行数据的判断, 如果是需要使用headers的地方可以进行立面使用
     if (option.url.indexOf('/my') !== -1) {
         option.headers = { Authorization: localStorage.getItem('token') }
-    }
-    option.complete = function (res) {
-        // 使用 res.responseJSON 获取到服务器的响应内容
-        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-            // 用户没有登录，就来访问 index 页面
-            // 1. 清空假 token
-            localStorage.removeItem('token')
-            // 2. 强制用户跳转到 登录页面
-            location.href = '/login.html'
+
+        option.complete = function (res) {
+            // 使用 res.responseJSON 获取到服务器的响应内容
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                // 用户没有登录，就来访问 index 页面
+                // 1. 清空假 token
+                localStorage.removeItem('token')
+                // 2. 强制用户跳转到 登录页面
+                location.href = '/login.html'
+            }
         }
     }
+
 
 
 })
